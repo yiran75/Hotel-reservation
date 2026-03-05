44a2d5241223d58c74b68b714dfb122f147b3b59
yiran75 <mohathirahmed55561@gmail.com>
2026-03-05 17:48:15 +0600

    Update Reservation.java

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,39 +1,22 @@
 import java.io.Serializable;
-import java.util.Date;
 
 public class Reservation implements Serializable {
-    private static final long serialVersionUID = 1L;
     private final String resId;
     private final String roomId;
     private final String custId;
-    private final Date checkIn;
-    private final Date checkOut;
     private final int total;
 
     private Reservation(Builder builder) {
         this.resId = builder.resId;
         this.roomId = builder.roomId;
         this.custId = builder.custId;
-        this.checkIn = builder.checkIn;
-        this.checkOut = builder.checkOut;
         this.total = builder.total;
     }
-
-    public String getResId() { return resId; }
-    public String getRoomId() { return roomId; }
-    public String getCustId() { return custId; }
-    public Date getCheckIn() { return checkIn; }
-    public Date getCheckOut() { return checkOut; }
-    public int getTotal() { return total; }
-
-    
 
     public static class Builder {
         private String resId;
         private String roomId;
         private String custId;
-        private Date checkIn;
-        private Date checkOut;
         private int total;
 
         public Builder setResId(String resId) {
@@ -48,16 +31,6 @@ public class Reservation implements Serializable {
 
         public Builder setCustId(String custId) {
             this.custId = custId;
-            return this;
-        }
-
-        public Builder setCheckIn(Date checkIn) {
-            this.checkIn = checkIn;
-            return this;
-        }
-
-        public Builder setCheckOut(Date checkOut) {
-            this.checkOut = checkOut;
             return this;
         }
 
