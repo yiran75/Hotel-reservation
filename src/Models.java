826190c2b50a136e8ce64b813aeddcd4b98b77be
yiran75 <mohathirahmed55561@gmail.com>
2026-03-05 19:24:40 +0600

    Update Models.java

--- src/Models.java
+++ src/Models.java
@@ -1,22 +1,33 @@
 
 import java.io.Serializable;
 
-class User implements Serializable {
-    String username, password, role;
-    User(String u, String p, String r) { username = u; password = p; role = r; }
+class Customer implements Serializable {
+    private String id, name, phone, email;
+
+    public Customer(String id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
 }
 
 class Room implements Serializable {
     private final String roomId, type;
     private final int price;
 
-    private Room(Builder b) { this.roomId = b.roomId; this.type = b.type; this.price = b.price; }
+    private Room(Builder b) {
+        this.roomId = b.roomId;
+        this.type = b.type;
+        this.price = b.price;
+    }
+
     public String getRoomId() { return roomId; }
-    public String getType() { return type; }
     public int getPrice() { return price; }
 
     public static class Builder {
-        private String roomId, type; private int price;
+        private String roomId, type;
+        private int price;
         public Builder setRoomId(String id) { this.roomId = id; return this; }
         public Builder setType(String t) { this.type = t; return this; }
         public Builder setPrice(int p) { this.price = p; return this; }
