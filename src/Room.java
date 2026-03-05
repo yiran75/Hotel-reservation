0d4e45bee6409e12f61c00fa06cf601179acc02a
yiran75 <mohathirahmed55561@gmail.com>
2026-03-05 11:52:02 +0600

    Update Room.java

--- src/Room.java
+++ src/Room.java
@@ -2,16 +2,16 @@ import java.io.Serializable;
 
 public class Room implements Serializable {
     private static final long serialVersionUID = 1L;
-    private String roomId;
-    private RoomType type;
-    private BedType bed;
-    private int price;
+    private final String roomId;
+    private final RoomType type;
+    private final BedType bed;
+    private final int price;
 
-    public Room(String roomId, RoomType type, BedType bed, int price) {
-        this.roomId = roomId;
-        this.type = type;
-        this.bed = bed;
-        this.price = price;
+    private Room(Builder builder) {
+        this.roomId = builder.roomId;
+        this.type = builder.type;
+        this.bed = builder.bed;
+        this.price = builder.price;
     }
 
     public String getRoomId() { return roomId; }
@@ -21,4 +21,20 @@ public class Room implements Serializable {
 
     @Override
     public String toString() { return roomId; }
+
+    public static class Builder {
+        private String roomId;
+        private RoomType type;
+        private BedType bed;
+        private int price;
+
+        public Builder setRoomId(String roomId) { this.roomId = roomId; return this; }
+        public Builder setType(RoomType type) { this.type = type; return this; }
+        public Builder setBed(BedType bed) { this.bed = bed; return this; }
+        public Builder setPrice(int price) { this.price = price; return this; }
+
+        public Room build() {
+            return new Room(this);
+        }
+    }
 }
